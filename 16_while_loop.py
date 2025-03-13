a0e41131373d979aad0212b0341154aef04070a5
KamalCdac <kamalkaur.cdac@gmail.com>
2025-03-13 15:16:14 +0530

    test

--- 16_while_loop.py
+++ 16_while_loop.py
@@ -1,19 +1,26 @@
 #while loop = execute some code while some condition remain true
 
 
-name = input("Enter your name? :")
-print("this line is added using mobile")
-while name == "":
-    print("You did not enter your name")
-    name = input("Enter your name? :")
-print(f"Hello {name}")
-
-food = input("Enter a food item you like (q to quit): ")
-
-while food!='q':
-    print(f"you like {food}")
-    food = input("Enter a food item you like (q to quit): ")
-print(f" Your name is {name}")
+# name = input("Enter your name? :")
+# print("this line is added using mobile")
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter your name? :")
+# print(f"Hello {name}")
+#
+# food = input("Enter a food item you like (q to quit): ")
+#
+# while not food == 'q':
+# #while food!='q':
+#     print(f"you like {food}")
+#     food = input("Enter a food item you like (q to quit): ")
+# print(f" Your name is {name}")
 
 # First commit changes
 # Second push changes
+
+num = int(input("Enter a number between 1 and 10: "))
+while num <1 or num >10:
+     print(f"{num} is not valid")
+     num = int(input("Enter a number between 1 and 10: "))
+print(f"You have entered {num}")
---
+++ 17_compound_interest_calculator.py
@@ -0,0 +1,66 @@
+# Python compound interest calculator
+
+principle = 0
+rate = 0
+time = 0
+
+while principle <= 0:
+    principle = float(input("Enter the principle amount: "))
+    if principle <= 0:
+        print("Principle can't be less than or equal to zero")
+
+
+while rate <= 0:
+    rate = float(input("Enter the rate amount: "))
+    if rate <= 0:
+        print("Interest rate can't be less than or equal to zero")
+
+
+
+while time <= 0:
+    time = float(input("Enter the time in years: "))
+    if time <= 0:
+        print("time can't be less than or equal to zero")
+
+
+print(principle)
+print(rate)
+print(time)
+
+total = principle * pow((1 + rate / 100),time)
+print(f"Compund interest is {total:.2f}")
+
+
+#----------------------------This code allows entering 0 input-------------------------------------------------------------------------------------
+
+while True:
+    principle = float(input("Enter the principle amount: "))
+    if principle < 0:
+        print("Principle can't be less than  zero")
+    else:
+        break
+
+
+while True:
+    rate = float(input("Enter the rate amount: "))
+    if rate < 0:
+        print("Interest rate can't be less than  zero")
+    else:
+        break
+
+
+
+while True:
+    time = float(input("Enter the time in years: "))
+    if time < 0:
+        print("time can't be less  zero")
+    else:
+        break
+
+
+print(principle)
+print(rate)
+print(time)
+
+total = principle * pow((1 + rate / 100),time)
+print(f"Compund interest is {total:.2f}")
