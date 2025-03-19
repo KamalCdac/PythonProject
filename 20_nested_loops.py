dd606e8adfee2cff37ab976dde5c167fcfca00d7
KamalCdac <kamalkaur.cdac@gmail.com>
2025-03-19 16:58:10 +0530

    collection

---
+++ 20_nested_loops.py
@@ -0,0 +1,17 @@
+# A loop within another loop is called nested loop (outer, inner)
+#                 outer loop:
+#                 inner loop:
+# if we want each character on same line use end keyword
+# we could use print() function also without any argument to print inner foreach contents on new line on every outer loop iteration
+
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter the symbol to print: ")
+
+for  y in range(rows):
+    for x in range(columns):
+        print(symbol, end=" ")
+#    print(end="\n") or
+    print()
+
+
