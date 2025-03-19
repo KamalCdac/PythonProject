31dc9185b9e191e703a88a71e13d711e1e729d41
KamalCdac <kamalkaur.cdac@gmail.com>
2025-03-19 17:52:13 +0530

    collection

---
+++ 21_collection.py
@@ -0,0 +1,5 @@
+# Collection = Single "variable" used to store multiple values
+# List  =  [] Ordered and Changeable. Duplicates OK.
+# Set   =  {} Unordered and Immutable, But Add/Remove OK. NO Duplicates.
+# Tuple =  () Ordered and Changeable. Duplicates OK. FASTER
+
