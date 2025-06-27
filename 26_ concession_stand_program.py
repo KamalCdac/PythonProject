9bebba98dbee485ddc438b2205469272fe3935f1
KamalCdac <kamalkaur.cdac@gmail.com>
2025-06-27 15:35:22 +0530

    collection

--- 26_ concession_stand_program.py
+++ 26_ concession_stand_program.py
@@ -14,5 +14,24 @@ cart = []
 
 total =0
 
+print("      MENU                       ")
 for key, value in menus.items():
     print(f"{key:10}:${value:.2f}")
+
+print("---------------------------------")
+
+while True:
+    food = input("Enter a food item (q to quit): ").lower()
+    if food == 'q':
+        break;
+    # elif not menus.get(food):
+    elif menus.get(food) is not None:
+        price = menus.get(food)
+        total += price
+        cart.append(food)
+    else:
+        print("this food item not available")
+
+for food in cart:
+    print(food, end=" ")
+print(f'Your total is {total:.2f}')
