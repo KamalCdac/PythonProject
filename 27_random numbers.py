6efc9e85f65427a2ae3ba65da83bed4591196261
KamalCdac <kamalkaur.cdac@gmail.com>
2025-06-27 17:26:00 +0530

    collection

--- 27_random numbers.py
+++ 27_random numbers.py
@@ -1,1 +1,17 @@
 #  random numbers
+
+import random
+
+# print(help(random))
+low = 1
+high =16
+
+number = random.randint(low,high)
+print(number)
+
+number1 = random.random() # floating point number between 0 and 1
+print(number1)
+
+options = ("salt","paper","scissor")
+
+print(random.choice(options))
